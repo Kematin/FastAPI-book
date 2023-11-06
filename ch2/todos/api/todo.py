06ec06b8aee00e90eaa656097c9c9d70a33e21d4
kematin <kematin@inbox.ru>
2023-11-06 16:49:17 +0500

    learn data validation fastapi

--- ch2/todos/api/todo.py
+++ ch2/todos/api/todo.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter
+from models import TodoItem
 
 todo_router = APIRouter()
 todo_list = list()
 
 
 @todo_router.post("/todo")
-async def add_task(todo: dict) -> dict:
+async def add_task(todo: TodoItem) -> dict:
     todo_list.append(todo)
     return {
         "message": "Todo added successfully."
